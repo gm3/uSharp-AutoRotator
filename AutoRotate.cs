cf98bbac919881a976a622abd35f4e25913acb7f
Godfrey Meyer <gm3beats@gmail.com>
2022-06-01 17:29:37 -0600

    add script

---
+++ AutoRotate.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+
+        
+
+public class AutoRotate : UdonSharpBehaviour
+{
+  
+  public float ySpeed = 0.3f;
+  public float xSpeed = 0f;
+  public float zSpeed = 0f;
+
+  public GameObject thingToRotate; 
+
+
+    void Update()
+   {
+       thingToRotate.transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+   }
+}
